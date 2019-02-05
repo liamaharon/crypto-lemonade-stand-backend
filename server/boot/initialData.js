1028fb5f8ec0dc744fc4591081e3ef6542cbb21f
Liam Aharon <liam.aharon@hotmail.com>
2019-02-05 16:31:27 +1100

    Improve initialData formatting

--- server/boot/initialData.js
+++ server/boot/initialData.js
@@ -1,43 +1,46 @@
 'use strict';
 
 module.exports = function(server) {
-  // Products
-  server.models.Product.create({
-    name: 'Bitcoin',
-    ticker: 'BTC',
-    details: 'Native cryptocurrency asset',
-  });
-  server.models.Product.create({
-    name: 'Ethereum',
-    ticker: 'ETH',
-    details: 'Native cryptocurrency asset',
-  });
+  server.models.Product.create([
+    {
+      name: 'Bitcoin',
+      ticker: 'BTC',
+      details: 'Native cryptocurrency asset',
+    },
+    {
+      name: 'Ethereum',
+      ticker: 'ETH',
+      details: 'Native cryptocurrency asset',
+    },
+  ]);
 
-  // Orders
-  server.models.Order.create({
-    qty: 5,
-    address: '0x00000000',
-    status: 'PENDING_PAYMENT',
-    productId: 2,
-    accountId: 1,
-  });
-  server.models.Order.create({
-    qty: 1.2,
-    address: '1BTC2',
-    status: 'PENDING_PAYMENT',
-    productId: 1,
-    accountId: 1,
-  });
+  server.models.Order.create([
+    {
+      qty: 5,
+      address: '0x00000000',
+      status: 'PENDING_PAYMENT',
+      productId: 2,
+      accountId: 1,
+    },
+    {
+      qty: 1.2,
+      address: '1BTC2',
+      status: 'PENDING_PAYMENT',
+      productId: 1,
+      accountId: 1,
+    },
+  ]);
 
-  // Accounts
-  server.models.Account.create({
-    email: 'acc1@email.com',
-    password: 'pass',
-    phoneNumber: '0400000000',
-  });
-  server.models.Account.create({
-    email: 'acc2@email.com',
-    password: 'pass',
-    phoneNumber: '0400000001',
-  });
+  server.models.Account.create([
+    {
+      email: 'acc1@email.com',
+      password: 'pass',
+      phoneNumber: '0400000000',
+    },
+    {
+      email: 'acc2@email.com',
+      password: 'pass',
+      phoneNumber: '0400000001',
+    },
+  ]);
 };
