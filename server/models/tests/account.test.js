66002250885096be74d7faa8b72dc6cf9a771308
Liam Aharon <liam.aharon@hotmail.com>
2019-02-05 14:15:33 +1100

    Improve acc tests

--- server/models/tests/account.test.js
+++ server/models/tests/account.test.js
@@ -14,4 +14,40 @@ describe('Account model', () => {
       expect(res.status).toEqual(200);
     });
   });
+
+  describe('POST Accounts', () => {
+    it('should fail if phoneNumber is blank', async () => {
+      const req = request
+        .post(`${endpoint}/accounts`)
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send({
+          email: 'acc@email.com',
+          password: 'pass',
+        });
+
+      expect(() => req()).toThrowError();
+    });
+
+    it('should sucessfully create new accounts', async () => {
+      const expected = {
+        email: 'INTERNALTEST@INTERNALTEST.COM',
+        phoneNumber: '04000',
+        verificationLevel: 0,
+        id: 1,
+      };
+      const res = await request
+        .post(`${endpoint}/accounts`)
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send({
+          email: 'INTERNALTEST@INTERNALTEST.COM',
+          password: 'pass',
+          phoneNumber: '04000',
+        });
+      expect(res.status).toEqual(200);
+
+      expect(JSON.parse(res.text)).toEqual(expected);
+    });
+  });
 });
