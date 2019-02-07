e4f6544d3826262f97302cc63a5f5dc8a9db4565
Liam Aharon <liam.aharon@hotmail.com>
2019-02-07 15:21:34 +1100

    Enforce status

--- server/models/order.js
+++ server/models/order.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function(Order) {
-
+  Order.validate('status', customValidator, {message: 'Invalid status'});
+  function customValidator(err) {
+    if
+    (
+      this.status !== 'PENDING_PAYMENT' &&
+      this.status !== 'PENDING_FUFILMENT' &&
+      this.status !== 'COMPLETED'
+    ) err();
+  };
 };
