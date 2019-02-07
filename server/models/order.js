057d08bb5cf8427fda29f9e4e0f33be14aec574b
Liam Aharon <liam.aharon@hotmail.com>
2019-02-07 15:55:10 +1100

    Enforce that ETH and BTC orders contain a valid address

--- server/models/order.js
+++ server/models/order.js
@@ -1,13 +1,26 @@
 'use strict';
 
+const validateAddress = require('wallet-address-validator').validate;
+
 module.exports = function(Order) {
-  Order.validate('status', customValidator, {message: 'Invalid status'});
-  function customValidator(err) {
+  // enforce valid status
+  Order.validate('status', statusValidator, {message: 'Invalid status'});
+  function statusValidator(err) {
     if
     (
       this.status !== 'PENDING_PAYMENT' &&
       this.status !== 'PENDING_FUFILMENT' &&
       this.status !== 'COMPLETED'
     ) err();
+  };
+
+  // enforce valid ETH and BTC addresses
+  Order.validate('address', addressValidator, {message: 'Invalid address'});
+  function addressValidator(err) {
+    if (this.productId === 1 && !validateAddress(this.address, 'BTC')) {
+      err();
+    } else if (this.productId === 2 && !validateAddress(this.address, 'ETH')) {
+      err();
+    }
   };
 };
